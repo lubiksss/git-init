22e7a13d5d6f6a5225ad51b050aaaf587bafac3b
lubiksss <lubiksss@gmail.com>
2024-01-24 17:45:12 +0900

    feat:

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ PRHOOK=".github"
 mkdir -p $GITHOOK
 mkdir -p $PRHOOK
 
-[ -e ./READMD.md ] && mv $MAIN/README.md ./README.md
-[ -e ./$GITHOOK/commit-msg ] && mv $MAIN/commit-msg ./$GITHOOK/commit-msg
-[ -e ./$PRHOOK/pull_request_template.md /READMD.md ] && mv $MAIN/pull_request_template.md ./$PRHOOK/pull_request_template.md
+[ ! -e ./READMD.md ] && mv $MAIN/README.md ./README.md
+[ ! -e ./$GITHOOK/commit-msg ] && mv $MAIN/commit-msg ./$GITHOOK/commit-msg
+[ ! -e ./$PRHOOK/pull_request_template.md ] && mv $MAIN/pull_request_template.md ./$PRHOOK/pull_request_template.md
 
 rm -rf MAIN
