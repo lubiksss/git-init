6574fcd79e3af8ed756bdf5168e2cc55cec5a0e9
lubiksss <lubiksss@gmail.com>
2024-01-24 17:32:26 +0900

    feat:

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+MAIN=git-init-main
+GITHOOK=.git/hooks
+PRHOOK=.github
 
+mkdir -p GITHOOK
+mkdir -p PRHOOK
+
+mv $MAIN/README.md ../README.md
+mv $MAIN/commit-msg ../$GITHOOK/commit-msg
+mv $MAIN/pull_request_template.md ../$PRHOOK/pull_request_template.md
+
+rm -rf MAIN
