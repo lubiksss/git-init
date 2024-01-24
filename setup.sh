13b26ef1e077dd3dd5bcc32e9fbda43685755df4
lubiksss <lubiksss@gmail.com>
2024-01-24 17:35:42 +0900

    feat:

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-MAIN=git-init-main
-GITHOOK=.git/hooks
-PRHOOK=.github
+MAIN="git-init-main"
+GITHOOK=".git/hooks"
+PRHOOK=".github"
 
-mkdir -p GITHOOK
-mkdir -p PRHOOK
+mkdir -p $GITHOOK
+mkdir -p $PRHOOK
 
 mv $MAIN/README.md ../README.md
 mv $MAIN/commit-msg ../$GITHOOK/commit-msg
